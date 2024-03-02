0b774673945ebe0600ba3cc6330bf3015855c540
yyhhenry <yyhhenry@foxmail.com>
2024-03-02 09:49:29 +0800

    Refactor build_data function and extract compile and zip_data functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,8 @@ use anyhow::{anyhow, Result};
 use std::{path::Path, process::Command};
 pub mod acm_rand;
 
-pub trait Generator {
-    fn generate(&mut self, id: usize, file: &mut std::fs::File) -> Result<()>;
-}
-pub fn build_data(
-    name: impl AsRef<str>,
-    ids: impl Iterator<Item = usize>,
-    mut generator: impl Generator,
-) -> Result<()> {
-    let name = name.as_ref();
-    let root_dir = Path::new(".").join("problems").join(name);
-    // check if the problem exists
-    if !root_dir.is_dir() {
-        return Err(anyhow!("Problem {} does not exist", name));
-    }
-    let std_exe = root_dir.join("std.exe");
+fn compile(std_exe: impl AsRef<Path>) -> Result<()> {
+    let std_exe = std_exe.as_ref();
     let compiling = Command::new("g++")
         .arg(&std_exe.with_extension("cpp"))
         .arg("-o")
@@ -29,6 +16,42 @@ pub fn build_data(
     if !compiling.success() {
         return Err(anyhow!("Failed to compile {}", std_exe.display()));
     }
+    Ok(())
+}
+fn zip_data(name: impl AsRef<str>, data_dir: impl AsRef<Path>) -> Result<()> {
+    let name = name.as_ref();
+    let data_dir = data_dir.as_ref();
+    let zip_file = data_dir.join("data.zip");
+    if zip_file.is_file() {
+        std::fs::remove_file(&zip_file)?;
+    }
+    let packing = Command::new("7z")
+        .current_dir(&data_dir)
+        .arg("a")
+        .arg(format!("{}-data.zip", name))
+        .arg("*.in")
+        .arg("*.out")
+        .status()?;
+    if !packing.success() {
+        return Err(anyhow!("Failed to pack data"));
+    }
+    Ok(())
+}
+pub trait Generator {
+    fn generate(&mut self, id: usize, file: &mut std::fs::File) -> Result<()>;
+}
+pub fn build_data(
+    name: impl AsRef<str>,
+    ids: impl Iterator<Item = usize>,
+    mut generator: impl Generator,
+) -> Result<()> {
+    let name = name.as_ref();
+    let root_dir = Path::new(".").join("problems").join(name);
+    if !root_dir.is_dir() {
+        return Err(anyhow!("Problem {} does not exist", name));
+    }
+    let std_exe = root_dir.join("std.exe");
+    compile(&std_exe)?;
     let data_dir = root_dir.join("data");
     if !data_dir.is_dir() {
         std::fs::create_dir(&data_dir)?;
@@ -49,23 +72,14 @@ pub fn build_data(
         let std_input = std::fs::File::open(&input_file)?;
         let std_output = std::fs::File::create(&output_file)?;
 
-        Command::new(&std_exe)
+        let std_running = Command::new(&std_exe)
             .stdin(std_input)
             .stdout(std_output)
             .status()?;
-
+        if !std_running.success() {
+            return Err(anyhow!("Failed to run {}", std_exe.display()));
+        }
         println!("Created output file {}", output_file.display());
     }
-    let zip_file = data_dir.join("data.zip");
-    if zip_file.is_file() {
-        std::fs::remove_file(&zip_file)?;
-    }
-    Command::new("7z")
-        .current_dir(&data_dir)
-        .arg("a")
-        .arg(format!("{}-data.zip", name))
-        .arg("*.in")
-        .arg("*.out")
-        .status()?;
-    Ok(())
+    zip_data(name, &data_dir)
 }
